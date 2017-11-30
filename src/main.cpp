7eeba4dac8e40bc2f9a3ac45277107a1054d0124
Cyril <brebisasthmatique@gmail.com>
2017-11-30 19:08:56 +0100

    J'ai fait n'imp (pardon)

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,24 @@
-#include <iostream>
+#include <SFML/Graphics.hpp>
 
-using namespace std;
+int main()
+{
+	sf::RenderWindow window(sf::VideoMode(200, 200), "SFML works!");
+	sf::CircleShape shape(100.f);
+	shape.setFillColor(sf::Color::Green);
 
-//coucou
+	while (window.isOpen())
+	{
+		sf::Event event;
+		while (window.pollEvent(event))
+		{
+			if (event.type == sf::Event::Closed)
+				window.close();
+		}
 
-void main(){
-	cout << "etonnant" << endl;
+		window.clear();
+		window.draw(shape);
+		window.display();
+	}
+
+	return 0;
 }
