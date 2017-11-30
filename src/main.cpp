7852cf704b3f020890608bf2c67d09c63eb94a66
Thomas <a.tom.hic3@gmail.com>
2017-11-30 18:49:22 +0100

    Ajout .gitignore et code de base SFML

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,21 @@
-#include <iostream>
+#include <SFML/Window.hpp>
 
-using namespace std;
+int main()
+{
+    sf::Window window(sf::VideoMode(800, 600), "My window");
 
-void main(){
-	cout << "etonnant" << endl;
+    // run the program as long as the window is open
+    while (window.isOpen())
+    {
+        // check all the window's events that were triggered since the last iteration of the loop
+        sf::Event event;
+        while (window.pollEvent(event))
+        {
+            // "close requested" event: we close the window
+            if (event.type == sf::Event::Closed)
+                window.close();
+        }
+    }
+
+    return 0;
 }
