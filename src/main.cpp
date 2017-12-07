b76d943e7e40ac51e6518b86db8da1359ed600a6
PierreTSE <pierre--giraud@orange.fr>
2017-12-07 14:24:46 +0100

    rc

--- src/main.cpp
+++ src/main.cpp
@@ -10,14 +10,14 @@
 int main()
 {
 	// Code propre 
-	/*
-	sf::RenderWindow window(sf::VideoMode(800, 600), " works!");
+	
+	sf::RenderWindow window(sf::VideoMode(800, 600), " Le plus grand jeu!");
 	Partie partie;
 	partie.jeu(&window);
-	*/
+	
 
 	//Bordel de Thomas
-
+	/*
 	sf::ContextSettings settings;
 	settings.antialiasingLevel = 8;
 	sf::RenderWindow window(sf::VideoMode(800, 800), "SFML works!", sf::Style::Default, settings);
@@ -100,7 +100,7 @@ int main()
 		window.draw(origine);
 		window.display();
 	}
-
+	*/
 	return 0;
 }
 
