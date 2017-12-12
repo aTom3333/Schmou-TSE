798173d7ec3d4aab56154c3de0649ae96c6cca8a
Thomas <a.tom.hic3@gmail.com>
2017-12-12 18:53:25 +0100

    Correction collisions

--- src/Collision.h
+++ src/Collision.h
@@ -6,8 +6,8 @@
 
 bool collision(const sf::Shape& s1, const sf::Shape& s2);
 bool collision_impl(const sf::Shape& s1, const sf::Shape& s2);
-bool collision(const sf::CircleShape& c1, const sf::CircleShape& c2);
-bool collision(const sf::CircleShape& c, const sf::Shape& co);
-bool collision(const sf::Shape& co, const sf::CircleShape& c);
+bool collision_impl(const sf::CircleShape& c1, const sf::CircleShape& c2);
+bool collision_impl(const sf::CircleShape& c, const sf::Shape& co);
+bool collision_impl(const sf::Shape& co, const sf::CircleShape& c);
 
 #endif // COLLISION_H
