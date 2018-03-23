021e06f7f68479ea6522327e643eb8b60719fe27
Cyril <brebisasthmatique@gmail.com>
2018-03-23 22:31:41 +0100

    Ecran titre et  Ecran chargement

--- src/Menu/Accueil.cpp
+++ src/Menu/Accueil.cpp
@@ -1,7 +1,23 @@
 #include "Accueil.h"
 
+Accueil::Accueil(sf::RenderWindow & window) : Ecran(window) 
+{
+	fondText_.loadFromFile("../../rc/Accueil/titre.png");
+	fond_.setTexture(fondText_);
+
+	texte_.setFont(polices_["hemi"]);
+	texte_.setString("Appuyez sur une touche");
+	texte_.setCharacterSize(32);
+	texte_.setPosition(ECRAN_L / 2 - texte_.getGlobalBounds().width / 2, 682);
+}
+
 ecran_t Accueil::executer()
 {
+	sf::Clock timer;
+	int alpha = 255;
+	int mult = -1;
+
+	auto t = timer.restart();
 	while (window_.isOpen())
 	{
 		// Gestion  des évènements 
@@ -18,9 +34,31 @@ ecran_t Accueil::executer()
 			}
 		}
 
-
 		// Efface l'écran
 		window_.clear();
+
+		t = timer.getElapsedTime();
+		if (t.asMilliseconds() > 50)
+		{
+			alpha += mult*10;
+
+			if (alpha < 0)
+			{
+				alpha = 0;
+				mult = -mult;
+			}
+			if (alpha > 255)
+			{
+				alpha = 255;
+				mult = -mult;
+			}
+
+			texte_.setFillColor({ 255, 255, 255, (sf::Uint8)alpha });
+			t = timer.restart();
+		}
+
+		window_.draw(fond_);
+		window_.draw(texte_);
 
 		// Mise à jour de l'écran
 		window_.display();
